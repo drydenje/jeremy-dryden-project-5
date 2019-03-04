392f48235fb3fac5a25b6dd9f613af8f9e5578c1
Jeremy Dryden <jeremy.dryden@gmail.com>
2019-03-04 12:43:00 -0500

    final push before deadline

--- src/components/Article.js
+++ src/components/Article.js
@@ -3,6 +3,7 @@ import './Article.css'
 
 const Article = (props) => {
    const date = new Date(props.article.date).toLocaleDateString('en-us');
+   const time =  new Date(props.article.date).toLocaleTimeString('us-eng');
    return (
       <article>
          { props.article.image ? 
@@ -13,8 +14,8 @@ const Article = (props) => {
          : ""
          }
          <h2><a href={props.article.link}>{props.article.title}</a></h2>
-         <h3>{props.article.source}</h3>
-         <h3>{date}</h3>
+         <h4>{props.article.source}</h4>
+         <h3>{date} - {time}</h3>
          <p>{props.article.desc}</p>
       </article>
    )
