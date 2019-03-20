6b68c0f93dd04a9e4b07631082403eec3616526f
Jeremy Dryden <jeremy.dryden@gmail.com>
2019-03-20 15:48:09 -0400

    working on the search bar move, need to style and add open menu button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react';
 import './Header.css';
 
-class Header extends Component {
-   render() {
-      return(
-         <header>
-            <div className="wrapper">
-               <h1>Feedwatch</h1>
-               <label htmlFor="navMenu">
-                  <i className="fas fa-bars"></i>
-               </label>
-            </div>
-         </header>
-      )
-   }
+const Header = (props) => {
+   return(
+      <header>
+         <div className="wrapper">
+            <h1>Feedwatch</h1>
+            <form action="submit" onSubmit={props.addQuery}>
+               <input type="text"
+                  placeholder="Add a query to watch for"
+                  name="addQuery"
+                  onChange={props.inputChange}
+               />
+               <button type="submit">Add Query</button>
+            </form>
+            <label htmlFor="navMenu">
+               {/* <i className="fas fa-bars"></i> */}
+               <a>test</a>
+            </label>
+         </div>
+      </header>
+   )
 }
 
 export default Header;
