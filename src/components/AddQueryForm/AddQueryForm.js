d6defaf18db421e1f4ac8add87fa76e962a95474
Jeremy Dryden <jeremy.dryden@gmail.com>
2022-06-04 23:37:39 -0400

    url work

--- src/components/AddQueryForm/AddQueryForm.js
+++ src/components/AddQueryForm/AddQueryForm.js
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 // import { useDispatch } from "react-redux";
 import { useSearchParams } from "react-router-dom";
-// import { nanoid } from "@reduxjs/toolkit";
-// import { addKeyword } from "../Navigation/navigationSlice";
+import { nanoid } from "@reduxjs/toolkit";
+import { addKeyword } from "../Navigation/navigationSlice";
 import "./AddQueryForm.css";
 
 // const query = ["Microsoft", "Apple", "IBM"];
@@ -12,21 +12,32 @@ const AddQueryForm = () => {
   const [keyword, setKeyword] = useState("");
   const onKeywordChanged = (e) => setKeyword(e.target.value);
   // const dispatch = useDispatch();
-  let [searchParams, setSearchParams] = useSearchParams();
+  // let [searchParams, setSearchParams] = useSearchParams();
+  const [searchParams] = useSearchParams();
+
+  useEffect(() => {
+    const currentParams = Object.fromEntries([...searchParams]);
+    console.log(currentParams["query"]);
+    // dispatchEvent(addKeyword({
+    //   id: nanoid(),
+    //   keyword:
+    // }))
+  }, [searchParams]);
 
   const handleSubmit = (event) => {
+    // console.log(event);
     event.preventDefault();
     if (keyword) {
-      // dispatch(
-      //   addKeyword({
-      //     id: nanoid(),
-      //     keyword,
-      //   })
-      // );
+      dispatch(
+        addKeyword({
+          id: nanoid(),
+          keyword: currentParams["query"],
+        })
+      );
       // setSearchParams(encodeURIComponent({ query }));
-      console.log(event.target);
-      let params = encodeURIComponent(event.target);
-      setSearchParams(params);
+      // console.log(event.target);
+      // let params = encodeURIComponent(keyword);
+      // setSearchParams(params);
       // console.log("Search:", searchParams);
       // setKeyword("");
     }
