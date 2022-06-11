068fd1fb7698cb5fee3ec84e7f1a5005f43517a3
Jeremy Dryden <jeremy.dryden@gmail.com>
2022-06-10 22:55:36 -0400

    rethinking the way the data will flow, unsure of the new redux way of doing this

--- src/components/AddQueryForm/AddQueryForm.js
+++ src/components/AddQueryForm/AddQueryForm.js
@@ -1,18 +1,15 @@
 import React, { useState, useEffect } from "react";
-// import { useDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
 import { useSearchParams } from "react-router-dom";
-// import { nanoid } from "@reduxjs/toolkit";
-// import { addKeyword } from "../Navigation/navigationSlice";
+import { nanoid } from "@reduxjs/toolkit";
+import { addArticles } from "../../containers/Feed/feedSlice";
 import { useNavigate } from "react-router-dom";
 import "./AddQueryForm.css";
-
-// const query = ["Microsoft", "Apple", "IBM"];
-// const query = { keyword: "Microsoft" };
 
 const AddQueryForm = () => {
   let navigate = useNavigate();
   const [keyword, setKeyword] = useState("");
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
   const onKeywordChanged = (e) => setKeyword(e.target.value);
   // let [searchParams, setSearchParams] = useSearchParams();
   let [searchParams] = useSearchParams();
@@ -22,19 +19,19 @@ const AddQueryForm = () => {
     console.log("Query:", currentParams["query"]);
     // check if a query has been provided
     // might have this check moved to the reducer?
-    // if (currentParams["query"]) {
-    //   dispatch(
-    //     addKeyword({
-    //       id: nanoid(),
-    //       keyword: currentParams["query"],
-    //     })
-    //   );
-    // }
+    if (currentParams["query"]) {
+      dispatch(
+        addArticles({
+          id: nanoid(),
+          keyword: currentParams["query"],
+        })
+      );
+    }
   }, [searchParams]);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    navigate(`./search/?query=${keyword}`);
+    navigate(`./search/?query=${keyword}`, { replace: true });
 
     // if (keyword) {
     // dispatch(
