d1964dc2635a9233f59d98eee3e2852804440a4e
Jeremy Dryden <jeremy.dryden@gmail.com>
2022-06-13 18:58:22 -0400

    cleaned up AddQueryForm, separated some logic to more clearly defind fucntionality. Closing issue #37

--- src/components/AddQueryForm/AddQueryForm.js
+++ src/components/AddQueryForm/AddQueryForm.js
@@ -1,44 +1,24 @@
-import React, { useState, useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useState } from "react";
 import { useSearchParams } from "react-router-dom";
-import { nanoid } from "@reduxjs/toolkit";
-import { addArticles } from "../../containers/Feed/feedSlice";
-// import { useNavigate } from "react-router-dom";
 import "./AddQueryForm.css";
 
 const AddQueryForm = () => {
-  // let navigate = useNavigate();
   const [keyword, setKeyword] = useState("");
-  const dispatch = useDispatch();
   const onKeywordChanged = (e) => setKeyword(e.target.value);
   let [searchParams, setSearchParams] = useSearchParams();
 
-  useEffect(() => {
-    console.log(searchParams.getAll("q"));
-    const currentParams = Object.fromEntries([...searchParams]);
-    // console.log("q:", currentParams["q"]);
-    // check if a query has been provided
-    // might have this check moved to the reducer?
-    if (currentParams["q"]) {
-      dispatch(
-        addArticles({
-          id: nanoid(),
-          keyword: currentParams["q"],
-        })
-      );
-    }
-  }, [searchParams]);
-
   const handleSubmit = async (event) => {
+    // prevent the form from refreshing the page
     event.preventDefault();
-    // let newParams = ["Sony", "Microsoft"];
+
+    // create a new array from the parameters in the url
     let newParams = searchParams.getAll("q");
+    // add the latest keyword
     newParams.push(keyword);
-
-    // console.log("NP:", newParams);
+    // set the new url
     setSearchParams({ q: newParams });
-    // navigate(`./search/?query=${keyword}`, { replace: true });
 
+    // clear the state and text input
     setKeyword("");
   };
 
