e539c8ce39e73a8659aacc03bb07c9e2a4fbfd4b
Jeremy Dryden <jeremy.dryden@gmail.com>
2022-07-01 20:43:14 -0400

    unused var was stopping deploy

--- src/state/feedSlice.js
+++ src/state/feedSlice.js
@@ -1,5 +1,5 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import articles from "fixtures/articles";
+// import articles from "fixtures/articles";
 
 const initialState = {
   keywords: {
